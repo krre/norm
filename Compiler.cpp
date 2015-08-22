2b7688b5c141714b6a545666033f5c0adda98e00
Vladimir Zarypov <krre31@gmail.com>
2015-08-22 12:22:25 +0300

    reading record from Functions table

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,6 +1,7 @@
 #include "Compiler.h"
 #include "global.h"
 #include <QFileInfo>
+#include <QtSql>
 
 Compiler::Compiler(const QString &filePath): filePath(filePath)
 {
@@ -11,6 +12,24 @@ void Compiler::run()
 {
     if (isFileExists(filePath)) {
         console(filePath.toStdString())
+        auto db = QSqlDatabase::addDatabase("QSQLITE", filePath);
+        db.setDatabaseName(filePath);
+        if (!db.open()) {
+             qDebug("Error occurred opening the database.");
+             qDebug("%s.", qPrintable(db.lastError().text()));
+             return;
+        }
+
+        QSqlQuery query(db);
+        query.prepare("SELECT * FROM Functions");
+        if (!query.exec()) {
+            qDebug("Error occurred querying.");
+            qDebug("%s.", qPrintable(db.lastError().text()));
+            return;
+        }
+        while (query.next()) {
+            qDebug() << query.value("name").toString() << query.value("body").toString();
+        }
     } else {
         console("File not exists: " << filePath.toStdString())
     }
