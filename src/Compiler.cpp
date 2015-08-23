4629134c3cf76c02e67e167b548e0d14f29218bf
Vladimir Zarypov <krre31@gmail.com>
2015-08-23 17:24:37 +0300

    print command is generated to LLVM SSA

--- src/Compiler.cpp
+++ src/Compiler.cpp
@@ -34,16 +34,40 @@ void Compiler::run()
             console(query.value("name").toString().toStdString() << " "
                     << query.value("command").toString().toStdString() << " "
                     << query.value("argument").toString().toStdString())
+
+            if (query.value("command").toString() == "print") {
+
+                llvm::LLVMContext& context = llvm::getGlobalContext();
+                llvm::Module* module = new llvm::Module("top", context);
+                llvm::IRBuilder<> builder(context);
+
+                llvm::FunctionType *funcType =
+                    llvm::FunctionType::get(builder.getInt32Ty(), false);
+                llvm::Function *mainFunc =
+                    llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
+
+                llvm::BasicBlock *entry = llvm::BasicBlock::Create(context, "entrypoint", mainFunc);
+                builder.SetInsertPoint(entry);
+
+                llvm::Value *printArg = builder.CreateGlobalStringPtr(query.value("argument").toString().toStdString() + "\n");
+
+                std::vector<llvm::Type *> putsArgs;
+                putsArgs.push_back(builder.getInt8Ty()->getPointerTo());
+                llvm::ArrayRef<llvm::Type*>  argsRef(putsArgs);
+
+                llvm::FunctionType *putsType =
+                  llvm::FunctionType::get(builder.getInt32Ty(), argsRef, false);
+                llvm::Constant *putsFunc = module->getOrInsertFunction("puts", putsType);
+
+                builder.CreateCall(putsFunc, printArg);
+                builder.CreateRetVoid();
+
+                module->dump();
+            }
         }
     } else {
         console("File not exists: " << filePath.toStdString())
     }
-
-    llvm::LLVMContext& context = llvm::getGlobalContext();
-    llvm::Module* module = new llvm::Module("top", context);
-    llvm::IRBuilder<> builder(context);
-
-    module->dump();
 }
 
 bool Compiler::isFileExists(const QString &filePath) {
