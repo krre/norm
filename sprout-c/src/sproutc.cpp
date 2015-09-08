ee6271b5504f6b89947ffcf9b9f808e89ca39b38
Vladimir Zarypov <krre31@gmail.com>
2015-09-08 08:59:40 +0300

    enabled LLVM again

--- sprout-c/src/sproutc.cpp
+++ sprout-c/src/sproutc.cpp
@@ -15,49 +15,45 @@ void Compiler::run()
     if (isFileExists(filePath)) {
         SproutDb sproutDb;
         sproutDb.open(filePath);
-        QVariantList moduleList = sproutDb.readRecords("SELECT * FROM Modules");
-        std::cout << "DEBUG: " << moduleList.count() << std::endl;
-        qDebug() << moduleList;
-        /*
-        QSqlQuery query(db);
-        query.prepare("SELECT * FROM Functions");
-        if (!query.exec()) {
-            console("Error occurred querying");
-            console(db.lastError().text().toStdString())
-            return;
+        QVariantList functionList = sproutDb.readRecords("SELECT * FROM Functions WHERE name='main'");
+        QString functionId = functionList.at(0).toMap().value("id").toString();
+        qDebug() << functionId;
+        QVariantList instructionList = sproutDb.readRecords(QString("SELECT * FROM Instructions WHERE functionId=%1").arg(functionId));
+        qDebug() << instructionList;
+        QString instructionId = instructionList.at(0).toMap().value("id").toString();
+        QString instruction = instructionList.at(0).toMap().value("name").toString();
+        qDebug() << instructionId << instruction;
+        QVariantList argumentList = sproutDb.readRecords(QString("SELECT * FROM Arguments WHERE instructionId=%1").arg(instructionId));
+        qDebug() << argumentList;
+        QString argument = argumentList.at(0).toMap().value("arg").toString();
+        qDebug() << argument;
+
+        if (instruction == "print") {
+
+            llvm::LLVMContext& context = llvm::getGlobalContext();
+            llvm::Module* module = new llvm::Module("top", context);
+            llvm::IRBuilder<> builder(context);
+
+            llvm::FunctionType* funcType = llvm::FunctionType::get(builder.getInt32Ty(), false);
+            llvm::Function* mainFunc = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
+
+            llvm::BasicBlock* entry = llvm::BasicBlock::Create(context, "entrypoint", mainFunc);
+            builder.SetInsertPoint(entry);
+
+            llvm::Value* printArg = builder.CreateGlobalStringPtr(argument.toStdString() + "\n");
+
+            std::vector<llvm::Type*> putsArgs;
+            putsArgs.push_back(builder.getInt8Ty()->getPointerTo());
+            llvm::ArrayRef<llvm::Type*> argsRef(putsArgs);
+
+            llvm::FunctionType* putsType = llvm::FunctionType::get(builder.getInt32Ty(), argsRef, false);
+            llvm::Constant* putsFunc = module->getOrInsertFunction("puts", putsType);
+
+            builder.CreateCall(putsFunc, printArg);
+            builder.CreateRetVoid();
+
+            module->dump();
         }
-        while (query.next()) {
-            console(query.value("name").toString().toStdString() << " "
-                    << query.value("command").toString().toStdString() << " "
-                    << query.value("argument").toString().toStdString())
-
-            if (query.value("command").toString() == "print") {
-
-                llvm::LLVMContext& context = llvm::getGlobalContext();
-                llvm::Module* module = new llvm::Module("top", context);
-                llvm::IRBuilder<> builder(context);
-
-                llvm::FunctionType* funcType = llvm::FunctionType::get(builder.getInt32Ty(), false);
-                llvm::Function* mainFunc = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
-
-                llvm::BasicBlock* entry = llvm::BasicBlock::Create(context, "entrypoint", mainFunc);
-                builder.SetInsertPoint(entry);
-
-                llvm::Value* printArg = builder.CreateGlobalStringPtr(query.value("argument").toString().toStdString() + "\n");
-
-                std::vector<llvm::Type*> putsArgs;
-                putsArgs.push_back(builder.getInt8Ty()->getPointerTo());
-                llvm::ArrayRef<llvm::Type*> argsRef(putsArgs);
-
-                llvm::FunctionType* putsType = llvm::FunctionType::get(builder.getInt32Ty(), argsRef, false);
-                llvm::Constant* putsFunc = module->getOrInsertFunction("puts", putsType);
-
-                builder.CreateCall(putsFunc, printArg);
-                builder.CreateRetVoid();
-
-                module->dump();
-            }
-        }*/
     } else {
         qDebug() << "File not exists: " << filePath;
     }
