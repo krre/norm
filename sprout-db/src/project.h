ba776a735aa3aa9dba27b42082c418bd308835b4
Vladimir Zarypov <krre31@gmail.com>
2015-09-16 18:08:37 +0300

    converted Project::create to static function

--- sprout-db/src/project.h
+++ sprout-db/src/project.h
@@ -7,9 +7,9 @@ class Project : public QObject
     Q_OBJECT
 public:
     Project();
-    Q_INVOKABLE bool create(const QString& path, const QString& projectName);
+    static bool create(const QString& path, const QString& projectName);
 
 private:
-    void initTables(const QSqlDatabase& db);
-    void initRecords(const QSqlDatabase& db, const QString& projectName);
+    static void initTables(const QSqlDatabase& db);
+    static void initRecords(const QSqlDatabase& db, const QString& projectName);
 };
