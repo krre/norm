7ffebfd851c3f42fa55ff30b7aa2932c219ac42f
Vladimir Zarypov <krre31@gmail.com>
2017-11-17 15:11:11 +0200

    Add minimal project

---
+++ Source/irbis.pro
@@ -0,0 +1,8 @@
+QT += core sql
+
+CONFIG += c++11
+TARGET = irbis
+TEMPLATE = app
+
+SOURCES += \
+    main.cpp
---
+++ Source/main.cpp
@@ -0,0 +1,9 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
+    app.setApplicationName("Irbis");
+    app.setApplicationVersion("0.1.0");
+
+    return EXIT_SUCCESS;
+}
