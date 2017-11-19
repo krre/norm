ef793e1449bbdb2394bd8257d0f5f4f65f9acbde
Vladimir Zarypov <krre31@gmail.com>
2017-11-19 12:04:34 +0200

    Add command line parser

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,37 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QDebug>
 #include "Core/Defines.h"
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
     app.setApplicationName(APP_NAME);
     app.setApplicationVersion(APP_VERSION_STR);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Irbis command line compiler");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("source", QCoreApplication::translate("main", "Source file to compile"));
+
+    parser.addOptions({
+        {{"o", "output"}, QCoreApplication::translate("main", "Output directory")},
+    });
+
+    parser.process(app);
+
+    if (argc == 1) {
+        parser.showHelp();
+    } else {
+        const QStringList args = parser.positionalArguments();
+        if (args.count() == 0) {
+            qDebug() << "Unknown source file";
+            parser.showHelp();
+        } else {
+            QString filePath = args.at(0);
+            qDebug() << "Compile" << filePath;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
