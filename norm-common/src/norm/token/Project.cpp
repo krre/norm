46245983ee97e7ba1434676e417e54d137f33e58
Vladimir Zarypov <krre31@gmail.com>
2023-07-09 12:56:51 +0300

    Replace Builder class by Project::read() method

--- norm-common/src/norm/token/Project.cpp
+++ norm-common/src/norm/token/Project.cpp
@@ -6,4 +6,8 @@ Project::Project(const std::string& name, Type type) : m_name(name), m_type(type
 
 }
 
+Project* Project::read(const std::string& filePath) {
+    return new Project("", Project::Type::Binary);
+}
+
 }
--- norm-common/src/norm/token/Project.h
+++ norm-common/src/norm/token/Project.h
@@ -16,6 +16,8 @@ public:
     const std::string& name() const { return m_name; }
     Type type() const { return m_type; }
 
+    static Project* read(const std::string& filePath);
+
 private:
     std::string m_name;
     Type m_type;
