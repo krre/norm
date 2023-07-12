bf27ceb231017cec7c163c2de3c2ca492f5c2f25
Vladimir Zarypov <krre31@gmail.com>
2023-07-12 19:57:35 +0300

    Add Identifier and Expression instances into AssignStatement token

--- norm-common/src/norm/token/statement/AssignStatement.cpp
+++ norm-common/src/norm/token/statement/AssignStatement.cpp
@@ -1,4 +1,6 @@
 #include "AssignStatement.h"
+#include "norm/token/Identifier.h"
+#include "norm/token/expression/Expression.h"
 
 namespace Norm {
 
@@ -6,8 +8,20 @@ AssignStatement::AssignStatement() {
 
 }
 
+AssignStatement::~AssignStatement() {
+
+}
+
 std::string AssignStatement::description() const {
     return "Assign statement";
+}
+
+void AssignStatement::setIdentifier(std::unique_ptr<Identifier> identifier) {
+    m_identifier = std::move(identifier);
+}
+
+void AssignStatement::setExpression(std::unique_ptr<Expression> expression) {
+    m_expression = std::move(expression);
 }
 
 }
--- norm-common/src/norm/token/statement/AssignStatement.h
+++ norm-common/src/norm/token/statement/AssignStatement.h
@@ -1,15 +1,28 @@
 #pragma once
 #include "Statement.h"
+#include <memory>
 
 namespace Norm {
+
+class Identifier;
+class Expression;
 
 class AssignStatement : public Statement {
 public:
     AssignStatement();
+    ~AssignStatement();
 
     std::string description() const override;
 
+    void setIdentifier(std::unique_ptr<Identifier> identifier);
+    Identifier* identifier() const { return m_identifier.get(); }
+
+    void setExpression(std::unique_ptr<Expression> expression);
+    Expression* expression() const { return m_expression.get(); }
+
 private:
+    std::unique_ptr<Identifier> m_identifier;
+    std::unique_ptr<Expression> m_expression;
 };
 
 }
