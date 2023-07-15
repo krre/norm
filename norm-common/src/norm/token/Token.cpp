ca3517db3f571d2c5f965da0359bc27ce9856661
Vladimir Zarypov <krre31@gmail.com>
2023-07-15 10:01:51 +0300

    Add node-tree system to token

--- norm-common/src/norm/token/Token.cpp
+++ norm-common/src/norm/token/Token.cpp
@@ -1,9 +1,85 @@
 #include "Token.h"
+#include <assert.h>
 
 namespace Norm {
 
-Token::Token(Token* parent) : m_parent(parent) {
+Token::Token(Token* parent) {
+    setParent(parent);
+}
 
+Token::~Token() {
+    setParent(nullptr);
+    deleteChildren();
+}
+
+void Token::setParent(Token* parent) {
+    if (m_parent == parent) return;
+
+    // Remove self from children of previous parent.
+    if (m_parent) {
+        m_parent->removeChild(this);
+    }
+
+    if (parent) {
+        parent->addChild(this);
+    }
+
+    m_parent = parent;
+}
+
+void Token::addChild(Token* child) {
+    assert(child);
+    m_children.push_back(child);
+    setupNewChild(child);
+}
+
+void Token::insertChild(size_t index, Token* child) {
+    assert(child);
+    assert(index >= 0 && index < m_children.size());
+    m_children.insert(m_children.begin() + index, child);
+    setupNewChild(child);
+}
+
+void Token::removeChild(Token* child) {
+    assert(child);
+    m_children.erase(std::remove(m_children.begin(), m_children.end(), child), m_children.end());
+    child->m_parent = nullptr;
+}
+
+Token* Token::removeChild(size_t index) {
+    Token* child = m_children.at(index);
+    assert(index >= 0 && index < m_children.size());
+    removeChild(child);
+    return child;
+}
+
+void Token::deleteChild(Token* child) {
+    assert(child);
+    removeChild(child);
+    delete child;
+}
+
+void Token::deleteChild(size_t index) {
+    assert(index >= 0 && index < m_children.size());
+    Token* child = removeChild(index);
+    delete child;
+}
+
+void Token::deleteChildren() {
+    for (auto child : m_children) {
+        delete child;
+    }
+
+    m_children.clear();
+}
+
+void Token::setupNewChild(Token* child) {
+    //  Remove child from children of previous parent.
+    if (child->m_parent) {
+        child->m_parent->removeChild(child);
+    }
+
+    child->m_parent = this;
 }
 
 }
--- norm-common/src/norm/token/Token.h
+++ norm-common/src/norm/token/Token.h
@@ -1,17 +1,43 @@
 #pragma once
 #include <string>
+#include <vector>
 
 namespace Norm {
 
 class Token {
 public:
-    Token(Token* parent = nullptr);
+    explicit Token() = default;
+    Token(const Token& other) = delete;
+    Token& operator=(const Token& other) = delete;
+
+    virtual ~Token();
 
     virtual std::string description() const = 0;
+
+protected:
+    explicit Token(Token* parent);
+
     Token* parent() const { return m_parent; }
+    void setParent(Token* parent);
+
+    const std::vector<Token*> children() const { return m_children; }
+
+    void addChild(Token* child);
+    void insertChild(size_t index, Token* child);
+
+    void removeChild(Token* child);
+    Token* removeChild(size_t index);
+
+    void deleteChild(Token* child);
+    void deleteChild(size_t index);
+
+    void deleteChildren();
 
 private:
+    void setupNewChild(Token* child);
+
     Token* m_parent = nullptr;
+    std::vector<Token*> m_children;
 };
 
 }
