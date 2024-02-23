c066c75107017a2a030d350e6e2d1b76188f2e2d
Vladimir Zarypov <krre31@gmail.com>
2024-02-23 16:57:54 +0200

    Add CLI parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(about = "Norm managing CLI", version, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Create {
+        #[arg(required = true)]
+        name: String,
+    },
+
+    Build,
+}
+
 fn main() {
-    println!("Norm started");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Create { name } => {
+            println!("Creating {name}");
+        }
+        Commands::Build => {
+            println!("Building");
+        }
+    }
 }
