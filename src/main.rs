1541800c902b2dd43711bea6cfedb66d7bcc2255
Vladimir Zarypov <krre31@gmail.com>
2024-02-25 15:31:42 +0200

    Add options to 'create' CLI command

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,15 @@ struct Cli {
 enum Commands {
     /// Create new project
     Create {
+        /// Project name
         #[arg(required = true)]
         name: String,
+        /// Binary target
+        #[arg(long)]
+        bin: bool,
+        /// Library target
+        #[arg(long)]
+        lib: bool,
     },
     /// Build project
     Build,
@@ -22,8 +29,8 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Create { name } => {
-            println!("Creating {name}");
+        Commands::Create { name, bin, lib } => {
+            println!("Creating {name} {bin} {lib}");
         }
         Commands::Build => {
             println!("Building");
