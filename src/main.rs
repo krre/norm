522e6d257f5da2dbd5e4d401bf949397035af9d4
Vladimir Zarypov <krre31@gmail.com>
2025-09-23 17:49:28 +0300

    Add command router

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,5 @@
-use clap::{Args, Parser, Subcommand};
-use norm::project;
-
-#[derive(Parser, Debug)]
-#[command(about = "Command-line interface (CLI) for managing Norm code", version, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    /// Create new project
-    Create {
-        /// Project name
-        #[arg(required = true)]
-        name: String,
-
-        #[command(flatten)]
-        target: Target,
-    },
-    /// Build project
-    Build,
-}
-
-#[derive(Args, Debug)]
-#[group(multiple = false)]
-struct Target {
-    /// Application target
-    #[arg(long)]
-    app: bool,
-    /// Library target
-    #[arg(long)]
-    lib: bool,
-}
-
 fn main() {
-    let cli = Cli::parse();
-
-    match cli.command {
-        Commands::Create { name, target } => {
-            if target.lib {
-                project::library::create(&name);
-            } else {
-                project::application::create(&name);
-            }
-        }
-        Commands::Build => {
-            println!("Building");
-        }
+    if let Err(err) = norm::router::route() {
+        eprintln!("{err}");
     }
 }
---
+++ src/norm/router.rs
@@ -0,0 +1,54 @@
+use crate::project;
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(about = "Command-line interface (CLI) for managing Norm code", version, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Create new project
+    Create {
+        /// Project name
+        #[arg(required = true)]
+        name: String,
+
+        #[command(flatten)]
+        target: Target,
+    },
+    /// Build project
+    Build,
+}
+
+#[derive(Args, Debug)]
+#[group(multiple = false)]
+struct Target {
+    /// Application target
+    #[arg(long)]
+    app: bool,
+    /// Library target
+    #[arg(long)]
+    lib: bool,
+}
+
+pub fn route() -> Result<(), String> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Create { name, target } => {
+            if target.lib {
+                project::library::create(&name);
+            } else {
+                project::application::create(&name);
+            }
+        }
+        Commands::Build => {
+            println!("Building");
+        }
+    }
+
+    Ok(())
+}
