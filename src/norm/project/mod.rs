b7cd2c0da3dd98c254f60b3c21cccfb9bc772702
Vladimir Zarypov <krre31@gmail.com>
2025-09-28 12:06:17 +0300

    Use building project from `normlib` crate

--- src/norm/project/mod.rs
+++ src/norm/project/mod.rs
@@ -11,21 +11,11 @@ pub fn create(name: &str, target: Target) -> Result<(), String> {
 }
 
 pub fn build() -> Result<(), String> {
-    if let Ok(res) = fs::exists(normlib::project::APP_FILE)
-        && res
-    {
-        build_project(normlib::project::APP_FILE);
-        return Ok(());
+    if let Err(err) = normlib::project::build() {
+        return Err(format!("Can't build project: {}", err.kind()));
     }
 
-    if let Ok(res) = fs::exists(normlib::project::LIB_FILE)
-        && res
-    {
-        build_project(normlib::project::LIB_FILE);
-        return Ok(());
-    }
-
-    Err(String::from("Directory is not Norm project"))
+    Ok(())
 }
 
 pub fn run() -> Result<(), String> {
@@ -37,9 +27,4 @@ pub fn run() -> Result<(), String> {
     }
 
     Err(String::from("Directory is not Norm application project"))
-}
-
-fn build_project(file_name: &str) {
-    println!("Build file {}", file_name);
-    fs::create_dir("build").ok();
 }
